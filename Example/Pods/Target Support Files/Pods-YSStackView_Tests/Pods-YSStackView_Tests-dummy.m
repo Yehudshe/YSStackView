e459cfabab82fd5605a0393529b318eee32b8950
Yehudshe <yehudasheleg@gmail.com>
2021-11-20 20:17:28 +0200

    Initial commit with cocopods

---
+++ Example/Pods/Target Support Files/Pods-YSStackView_Tests/Pods-YSStackView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YSStackView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YSStackView_Tests
+@end
---
+++ Example/Pods/Target Support Files/YSStackView/YSStackView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YSStackView : NSObject
+@end
+@implementation PodsDummy_YSStackView
+@end
